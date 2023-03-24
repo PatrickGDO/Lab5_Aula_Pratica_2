f8e1cf9058aa5eb72e8c24e328202eb24bb929d7
PatrickGDO <128257785+PatrickGDO@users.noreply.github.com>
2023-03-24 10:39:34 -0300

    Add files via upload

---
+++ LAB2P/AT5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define par(a) ((a%2) = ?0:1) 
+
+int main(){
+    int a;
+    printf("Digite um numero inteiro : ");
+    scanf("%d",&a);
+    if(a%2=){
+        printf("O numero e Par!!");
+    }
+    else{
+        printf(" O numero e Impar!!");
+    }
+    return 0;
+}
